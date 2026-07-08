d4f6b9a9965b37ee938ec63bf5570735ecf77499
Gregory DeRoeck <40666@nelnet.net>
2026-07-08 15:15:48 -0600

    Added console app for first exercise.

--- Exercise1-EvenNumbers/Program.cs
+++ Exercise1-EvenNumbers/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var intArray = GetEvenNumbers(17, 4); //Result = [4, 8, 12, 16]
+            //var intArray = GetEvenNumbers(21, 7); //Result = [14]
+            //var intArray = GetEvenNumbers(5, 20); //Result = [10, 20]
+            //var intArray = GetEvenNumbers(3, 10); //Result = [6]
+            //var intArray = GetEvenNumbers(2, 2); //Result = [2]
+
+            foreach (var i in intArray)
+            {
+                Console.Write($"{i}, ");
+            }
+        }
+
+        private static int[] GetEvenNumbers(int num1, int num2)
+        {
+            var nums = new List<int>();
+            int start, end;
+
+            if (num1 < num2)
+            {
+                start = num2;
+                end = num1;
+            }
+            else
+            {
+                start = num1;
+                end = num2;
+            }
+
+            for (int i = 2; i <= start; i++)
+            {
+                if (i % end == 0 && i % 2 == 0)
+                {
+                    nums.Add(i);
+                }
+            }
+
+            return nums.ToArray();
+
         }
     }
 }
